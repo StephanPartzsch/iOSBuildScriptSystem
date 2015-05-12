7a19447a0c929d4c80c9f8f033f01c331c2616be
Stephan Partzsch <stephan.partzsch@dvag.com>
2015-05-12 13:40:31 +0200

    Fixes tests.

--- JenkinsTestTests/JenkinsTestTests.m
+++ JenkinsTestTests/JenkinsTestTests.m
@@ -34,14 +34,7 @@
 - (void)testMath {
     // This is an example of a functional test case.
     XCTAssert(YES, @"Pass");
-    XCTAssertEqual(11, 5+5);
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+    XCTAssertEqual(10, 5+5);
 }
 
 @end
