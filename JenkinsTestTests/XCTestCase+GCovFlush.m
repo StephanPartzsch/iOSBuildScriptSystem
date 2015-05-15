b898557a43ec074f2505464fbb251fd9e24317e1
Stephan Partzsch <stephan.partzsch@dvag.com>
2015-05-15 16:16:42 +0200

    Adds test case category to generate code coverage data.

---
+++ JenkinsTestTests/XCTestCase+GCovFlush.m
@@ -0,0 +1,29 @@
+#ifdef ENABLE_GCOV_FLUSH
+
+@import XCTest;
+@import ObjectiveC.runtime;
+
+
+extern void __gcov_flush();
+
+
+@implementation XCTestCase (GCovFlush)
+
++ (void)load {
+    Method original, swizzled;
+    
+    original = class_getClassMethod(self, @selector(tearDown));
+    swizzled = class_getClassMethod(self, @selector(_swizzledTearDown));
+    method_exchangeImplementations(original, swizzled);
+}
+
++ (void)_swizzledTearDown {
+    if (__gcov_flush) {
+        __gcov_flush();
+    }
+    [self _swizzledTearDown];
+}
+
+@end
+
+#endif
