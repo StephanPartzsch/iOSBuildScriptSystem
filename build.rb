d3520f54a6d8320e66ba9fb0154386db6e1e9173
Jenkins <jenkins@atlnx0060.id.dvag.com>
2015-05-26 16:04:03 +0200

    Work on build script.

--- build.rb
+++ build.rb
@@ -1,12 +1,19 @@
 #!/usr/bin/ruby
 
+PROJECT_NAME = `${PROJECT_NAME}`
+PROJECT_NAME2 = `echo ${PROJECT_NAME}`
 
 unless defined? PROJECT_NAME
 	PROJECT_NAME = 'HUND'
 end
 
-print PROJECT_NAME
 
 print ' ---------------- '
+
+print PROJECT_NAME
+print PROJECT_NAME2
+
+print ' ---------------- '
+
 
 exec 'echo "~Parameter: ${JOB_NAME}"'
