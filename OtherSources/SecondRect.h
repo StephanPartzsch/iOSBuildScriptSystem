b225eafe59ec0961312f207e50fc20945390bf6b
Stephan Partzsch <stephan.partzsch@dvag.com>
2015-05-28 16:40:25 +0200

    Adds new source code in different folder.

---
+++ OtherSources/SecondRect.h
@@ -0,0 +1,13 @@
+//
+//  SecondRect.h
+//  JenkinsTest
+//
+//  Created by Stephan Partzsch on 28/05/15.
+//  Copyright (c) 2015 Stephan Partzsch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondRect : UIView
+
+@end
---
+++ OtherSources/SecondRect.m
@@ -0,0 +1,37 @@
+//
+//  SecondRect.m
+//  JenkinsTest
+//
+//  Created by Stephan Partzsch on 28/05/15.
+//  Copyright (c) 2015 Stephan Partzsch. All rights reserved.
+//
+
+#import "SecondRect.h"
+
+@implementation SecondRect
+
+- (instancetype) init
+{
+    self = [super init];
+    if (self) {
+        self.backgroundColor = [UIColor yellowColor];
+        self.frame = [self getFrame];
+    }
+    
+    return self;
+}
+
+- (CGRect)getFrame
+{
+   return CGRectMake(0, 0, 100, 100);
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
