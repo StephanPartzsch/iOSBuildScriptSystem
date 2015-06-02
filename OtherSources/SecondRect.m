d0c1ba34658bd6e62ef13e8283501238b160b96b
Stephan Partzsch <stephan.partzsch@dvag.com>
2015-06-02 10:21:04 +0200

    Adds debbuging stuff.

--- OtherSources/SecondRect.m
+++ OtherSources/SecondRect.m
@@ -14,15 +14,29 @@
 {
     self = [super init];
     if (self) {
+        NSLog(@"AAA %@: %@", NSStringFromSelector(_cmd), self);
+        
         self.backgroundColor = [UIColor yellowColor];
         self.frame = [self getFrame];
+        
+        [self doSomething];
     }
     
     return self;
 }
 
+-(void) doSomething
+{
+    for (int i=0; i<50; i++) {
+        NSLog(@"Iteration# %i", i);
+    }
+}
+
 - (CGRect)getFrame
 {
+    NSString *string = [NSString string]; //@"HAllo unused";
+    
+    
    int number = 3;
    return CGRectMake(0, 0, 100, 100);
 }
