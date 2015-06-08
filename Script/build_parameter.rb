6b9e536993f2a73cb0b9eaa98ab40b90b750d767
Stephan Partzsch <stephan.partzsch@dvag.com>
2015-06-08 10:28:58 +0200

    Adds option for version suffix to file name.

--- Script/build_parameter.rb
+++ Script/build_parameter.rb
@@ -15,23 +15,24 @@ end
 
 
 
-@workspace					= '/Users/jenkins/.jenkins/jobs/JenkinsTestJob/workspace'
-@build_directory_path 		= @workspace + '/build'
-@reports_directory_path 	= @workspace + '/reports'
-@info_plist_path			= @workspace + '/JenkinsTest/Info.plist'
+@workspace						= '/Users/jenkins/.jenkins/jobs/JenkinsTestJob/workspace'
+@build_directory_path 			= @workspace + '/build'
+@reports_directory_path 		= @workspace + '/reports'
+@info_plist_path				= @workspace + '/JenkinsTest/Info.plist'
 
-@project_name 				= 'JenkinsTest'
-@scheme 					= 'JenkinsTest'
-@configuration 				= 'Release'
+@project_name 					= 'JenkinsTest'
+@scheme 						= 'JenkinsTest'
+@configuration 					= 'Release'
 
-@file_name_base				= @project_name
-@file_name_suffix			= '-V'
-@app_version				= get_app_version
-@file_name 					= @file_name_base + @file_name_suffix + @app_version
+@bundle_identifier				= 'de.sp.test.neo2015'
 
-@bundle_identifier			= 'de.sp.test.neo2015'
+@source_directories 			= 'JenkinsTest' 						# Separated by single space " "
 
-@source_directories 		= 'JenkinsTest' 						# Separated by single space " "
+@user_defined_arguments 		= 'DV_DISPLAY_NAME=DVAG-DEV BLA=test'	# Separated by single space " "
+@preprocessor_macros 			= 'COLOR_RED=1 OUTLINE=1'				# Separated by single space " "
 
-@user_defined_arguments 	= 'DV_DISPLAY_NAME=DVAG-DEV BLA=test'	# Separated by single space " "
-@preprocessor_macros 		= 'COLOR_RED=1 OUTLINE=1'				# Separated by single space " "
+@file_name						= @project_name
+@append_version_to_file_name	= true
+@version_prefix					= '-V'
+@app_version					= get_app_version
+@version_suffix					= '-dev-SDK8'
--- Script/rakefile.rb
+++ Script/rakefile.rb
@@ -2,6 +2,7 @@
 
 load 'build_parameter.rb'
 
+load 'build/set_plist_values.rb'
 load 'build/test_app.rb'
 load 'build/build_app.rb'
 load 'build/sign_app.rb'
@@ -21,6 +22,11 @@ namespace :build do
 	desc "Prepares and logs neccessary build parameters (set/overwrite)"
 	task :setup_parameters do
 		setup_parameters 
+	end
+	
+	desc "Sets up info-plist values"
+	task :set_plist_values do
+		set_plist_values
 	end
 	
 	desc "Sets up relevant metadata in plist files"
@@ -49,7 +55,7 @@ namespace :build do
 	end
 	
 	desc "Runs the whole build prozess (setup|clean|test|build|sign|distribute)"
-	task :ALL => [:setup_parameters, :setup_metadata, :test, :build, :sign, :distribute] do
+	task :ALL => [:setup_parameters, :set_plist_values, :setup_metadata, :test, :build, :sign, :distribute] do
 		puts "\n\n✅  Everything done!"
 		puts "==================================="
 		puts ""
@@ -90,69 +96,85 @@ def setup_parameters
 	puts "-----------------------------------"
 	
 	# Print current Xcode verion
-	puts "🔹  Xcode version \t\t" + `xcodebuild -version`.gsub("\n", "  ")
-	puts "🔹  Xcode path \t\t\t" + `xcode-select -print-path`
+	puts "🔹  Xcode version \t\t\t" + `xcodebuild -version`.gsub("\n", "  ")
+	puts "🔹  Xcode path \t\t\t\t" + `xcode-select -print-path`
 	puts "---\n\n"
 	
 	# Print build parameter
 	puts "➔  Overwrite the following parameters (🔸 ) in Jenkins build job. Use the parameter name in [brackets]"
 	puts "---"
 	
-	puts "🔹  Workspace \t\t\t" + @workspace
-	puts "🔹  Reports directory path \t" + @reports_directory_path
+	puts "🔹  Workspace \t\t\t\t" + @workspace
+	puts "🔹  Reports directory path \t\t" + @reports_directory_path
 	
 	build_directory_path = `echo "$BUILD_DIRECTORY_PATH"`.strip
 	if !build_directory_path.empty?; @build_directory_path = build_directory_path end
-	puts "🔸  [BUILD_DIRECTORY_PATH] \t" + @build_directory_path + ""
+	puts "🔸  [BUILD_DIRECTORY_PATH] \t\t" + @build_directory_path + ""
 	
 	info_plist_path = `echo "$INFO_PLIST_PATH"`.strip
 	if !info_plist_path.empty?; @info_plist_path = info_plist_path end
-	puts "🔸  [INFO_PLIST_PATH] \t\t" + @info_plist_path + ""
+	puts "🔸  [INFO_PLIST_PATH] \t\t\t" + @info_plist_path + ""
+	
 	puts ""
 	
 	project_name = `echo "$PROJECT_NAME"`.strip
 	if !project_name.empty?; @project_name = project_name end
-	puts "🔸  [PROJECT_NAME] \t\t" + @project_name + ""
+	puts "🔸  [PROJECT_NAME] \t\t\t" + @project_name + ""
 	
 	scheme = `echo "$SCHEME"`.strip
 	if !scheme.empty?; @scheme = scheme end
-	puts "🔸  [SCHEME] \t\t\t" + @scheme + ""
+	puts "🔸  [SCHEME] \t\t\t\t" + @scheme + ""
 	
 	configuration = `echo "$CONFIGURATION"`.strip
 	if !configuration.empty?; @configuration = configuration end
-	puts "🔸  [CONFIGURATION] \t\t" + @configuration + ""
+	puts "🔸  [CONFIGURATION] \t\t\t" + @configuration + ""
+	
 	puts ""
 	
-	file_name_base = `echo "$FILE_NAME_BASE"`.strip
-	if !file_name_base.empty?; @file_name_base = file_name_base end
-	puts "🔸  [FILE_NAME_BASE] \t\t" + @file_name_base + ""
+	file_name = `echo "$FILE_NAME"`.strip
+	if !file_name.empty?; @file_name = file_name end
+	puts "🔸  [FILE_NAME] \t\t\t\t" + @file_name + ""
 	
-	file_name_suffix = `echo "$FILE_NAME_SUFFIX"`.strip
-	if !file_name_suffix.empty?; @file_name_suffix = file_name_suffix end
-	puts "🔸  [FILE_NAME_SUFFIX] \t\t" + @file_name_suffix + ""
+	append_version_to_file_name = `echo "$APPEND_VERSION_TO_FILE_NAME"`.strip
+	if !append_version_to_file_name.empty?; @append_version_to_file_name = append_version_to_file_name end
+	puts "🔸  [APPEND_VERSION_TO_FILE_NAME]\t" 	+ @append_version_to_file_name.to_s + ""
 	
-	puts "🔹  App version \t\t\t" + @app_version + ""
+	if @append_version_to_file_name
+		
+		version_prefix = `echo "$VERSION_PREFIX"`.strip
+		if !version_prefix.empty?; @version_prefix = version_prefix end
+		puts "🔸  [VERSION_PREFIX] \t\t\t" + @version_prefix + ""
+		
+		puts "🔹  App version \t\t\t\t" + @app_version + ""
+		
+		version_suffix = `echo "$VERSION_SUFFIX"`.strip
+		if !version_suffix.empty?; @version_suffix = version_suffix end
+		puts "🔸  [VERSION_SUFFIX] \t\t\t" + @version_suffix + ""
+		
+		@file_name = @file_name + @version_prefix + @app_version + @version_suffix
+	end
 	
-	puts "🔹  File name (app/ipa) \t\t" + @file_name + ""
+	puts "🔹  Final file name \t\t\t" + @file_name + ""
+	
 	puts ""
 
 	bundle_identifier = `echo "$BUNDLE_IDENTIFIER"`.strip
 	if !bundle_identifier.empty?; @bundle_identifier = bundle_identifier end
-	puts "🔸  [BUNDLE_IDENTIFIER] \t\t" + @bundle_identifier + ""
+	puts "🔸  [BUNDLE_IDENTIFIER] \t\t\t" + @bundle_identifier + ""
 	puts ""
 	
 	source_directories = `echo "$SOURCE_DIRECTORIES"`.strip
 	if !source_directories.empty?; @source_directories = source_directories end
-	puts "🔸  [SOURCE_DIRECTORIES] \t" + @source_directories + ""
+	puts "🔸  [SOURCE_DIRECTORIES] \t\t" + @source_directories + ""
 	puts ""
 
 	user_defined_arguments = `echo "$USER_DEFINED_ARGUMENTS"`.strip
 	if !user_defined_arguments.empty?; @user_defined_arguments = user_defined_arguments end
-	puts "🔸  [USER_DEFINED_ARGUMENTS] \t" + @user_defined_arguments + ""
+	puts "🔸  [USER_DEFINED_ARGUMENTS] \t\t" + @user_defined_arguments + ""
 	
 	preprocessor_macros = `echo "$PREPROCESSOR_MACROS"`.strip
 	if !preprocessor_macros.empty?; @preprocessor_macros = preprocessor_macros end
-	puts "🔸  [PREPROCESSOR_MACROS] \t" + @preprocessor_macros + ""
+	puts "🔸  [PREPROCESSOR_MACROS] \t\t" + @preprocessor_macros + ""
 end
 
 
@@ -172,7 +194,4 @@ def setup_metadata
 		puts "DVAGBuildTime\t\t = #{build_date}"
 		`/usr/libexec/PlistBuddy -c 'Set :DVAGBuildTime #{build_date}' #{@workspace}/BuildMetadata.plist`
 	end
-	
-	`/usr/libexec/PlistBuddy -c 'Set :CFBundleIdentifier #{@bundle_identifier}' #{@info_plist_path}`
-	puts "CFBundleIdentifier\t = #{@bundle_identifier}"
 end
