aa0c41415eda00907c07302cfe19558528fc0e3b
Stephan Partzsch <stephan.partzsch@dvag.com>
2015-06-16 11:27:34 +0200

    Moves helper methods to separate file.

--- Script/build_scripts/build_process.rb
+++ Script/build_scripts/build_process.rb
@@ -10,6 +10,8 @@ require_relative 'build_steps/distribute_app.rb'
 
 require_relative 'metrics/count_lines.rb'
 
+require_relative 'helper.rb'
+
 
 
 
@@ -89,35 +91,4 @@ namespace :analyze do
 		puts "==================================="
 		puts ""
 	end
-end
-
-
-
-
-
-##
-# Helper methods
-##
-
-def get_app_version
-	app_version = `/usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' #{@info_plist_path}`.strip
-	return app_version
-end
-
-def get_workspace_or_set_it_with_path(workspace_path)
-	workspace = `echo "$WORKSPACE"`.strip
-	if workspace.empty?; workspace = workspace_path end
-	return workspace
-end
-
-def get_final_file_name
-	final_file_name = @file_name
-	
-	if @append_version_to_file_name
-		final_file_name += @version_prefix + @app_version + @version_suffix
-	end
-	
-	puts "📄  Final file name: " + final_file_name + "\n\n"
-	
-	return final_file_name
 end
---
+++ Script/build_scripts/helper.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+
+
+
+##
+# Helper methods
+##
+
+
+def get_app_version
+	app_version = `/usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' #{@info_plist_path}`.strip
+	return app_version
+end
+
+
+def get_workspace_or_set_it_with_path(workspace_path)
+	workspace = `echo "$WORKSPACE"`.strip
+	if workspace.empty?; workspace = workspace_path end
+	return workspace
+end
+
+
+def get_final_file_name
+	final_file_name = @file_name
+	
+	if @append_version_to_file_name
+		final_file_name += @version_prefix + @app_version + @version_suffix
+	end
+	
+	puts "📄  Final file name: " + final_file_name + "\n\n"
+	return final_file_name
+end
